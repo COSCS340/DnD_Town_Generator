d0e47bd8723d925d82ecd5f86bd5a72cc5596947
Ben Johnson <thisguyistotallyben@gmail.com>
2018-02-26 17:56:36 -0500

    townwizard recursively adds content on folder double click

--- backend/townwizard.py
+++ backend/townwizard.py
@@ -3,14 +3,14 @@
 #          compilation of town elements
 # Author:  Ben Johnson
 
+
+import os, sys, glob
 from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtWidgets import *
 from town import Town
 
-import sys
 
-
-class Wizard(QWidget):
+class Wizard(QtWidgets.QMainWindow):
     def __init__(self):
         # variables
         super().__init__()
@@ -20,7 +20,7 @@ class Wizard(QWidget):
         self.width = 600
         self.height = 400
         self.town = Town()
-  
+
         # init
         self.initUI()
  
@@ -31,14 +31,15 @@ class Wizard(QWidget):
         # variables
         self.models = {}
         self.widgets = {}
-
+        
         # add elements and setup layout
-        self.setup()
+        self.setupInterface()
+        self.setupMenu()
         
         # show the window
         self.show()
 
-    def setup(self):
+    def setupInterface(self):
         # layout
         gridlayout = QGridLayout()
         
@@ -51,10 +52,15 @@ class Wizard(QWidget):
         self.widgets['build'] = QPushButton('Build Town')
         self.widgets['newtown'] = QPushButton('New Town')
         self.widgets['loadtown'] = QPushButton('Load Town')
-        
+        self.widgets['townlabel'] = QLabel(self)
+        self.widgets['info'] = {}
+        self.widgets['info']['townname'] = "New Town"
+
         # models
         self.models['tree'] = QFileSystemModel()
 
+        # town name label widget setup
+        self.widgets['townlabel'].setText("WHEE THIS IS SOME TEXT")
         # file browser widget setup
         self.models['tree'].setRootPath('')
         self.widgets['tree'].setModel(self.models['tree'])
@@ -74,19 +80,52 @@ class Wizard(QWidget):
         self.widgets['build'].clicked.connect(self.buildsig)
 
         # add to grid layout
-        gridlayout.addWidget(self.widgets['tree'],0,0,2,3)
-        gridlayout.addWidget(self.widgets['new'],2,0)
-        gridlayout.addWidget(self.widgets['edit'],2,1)
-        gridlayout.addWidget(self.widgets['add'],2,2)
-        gridlayout.addWidget(self.widgets['newtown'],0,3)
-        gridlayout.addWidget(self.widgets['loadtown'],0,4)
-        gridlayout.addWidget(self.widgets['selected'],1,3,1,2)
-        gridlayout.addWidget(self.widgets['build'],2,4)
+        gridlayout.addWidget(self.widgets['tree'],0,0,1,3)
+        gridlayout.addWidget(self.widgets['new'],1,0)
+        gridlayout.addWidget(self.widgets['edit'],1,1)
+        gridlayout.addWidget(self.widgets['add'],1,2)
+        gridlayout.addWidget(self.widgets['selected'],0,3)
+        gridlayout.addWidget(self.widgets['build'],1,3)
+        #gridlayout.addWidget(self.widgets['townlabel'],0,5)
         gridlayout.setColumnStretch(3, 4)
-        gridlayout.setColumnStretch(4, 4)
+        #gridlayout.setColumnStretch(4, 4)
         
         # set layout
-        self.setLayout(gridlayout)
+        #self.setLayout(gridlayout)
+        
+        cw = QWidget(self)
+        self.setCentralWidget(cw)
+        cw.setLayout(gridlayout)
+
+    def setupMenu(self):
+        self.topbar = self.menuBar()
+        self.status = self.statusBar()
+        
+        # status bar
+        self.status.showMessage("Welcome. Load a town or start fresh.")
+        
+        # file menu
+        file_menu = self.topbar.addMenu('File')
+        file_open = QtWidgets.QAction('Open', self)
+        file_new = QtWidgets.QAction('New', self)
+        file_close = QtWidgets.QAction('Close', self)
+        file_build = QtWidgets.QAction('Build', self)
+        file_menu.addAction(file_open)
+        file_menu.addAction(file_new)
+        file_menu.addSeparator()
+        file_menu.addAction(file_build)
+        file_menu.addSeparator()
+        file_menu.addAction(file_close)
+        
+        file_open.triggered.connect(self.loadsig)
+        file_new.triggered.connect(self.newsig)
+        file_build.triggered.connect(self.buildsig)
+        
+        # edit menu
+        edit_menu = self.topbar.addMenu('Edit')
+        
+        # about menu
+        about_menu = self.topbar.addMenu('Help')
 
     ## signals
     
@@ -95,23 +134,8 @@ class Wizard(QWidget):
         index = self.widgets['tree'].currentIndex()
         fullname = self.models['tree'].filePath(index)
         
-        # get partial file name and type
-        prettyname = fullname[(fullname.rfind('/') + 1):]
-        ftype = prettyname[:prettyname.find('.')]
-        
-        # check type
-        
-        # event
-        if ftype == 'event':
-            # if not already selected
-            if prettyname not in self.town.events:
-                self.town.events[prettyname] = fullname
-                print(prettyname)
-        # townspeople  
-        elif ftype == 'townspeople':
-            if prettyname not in self.town.townspeople:
-                self.town.townspeople[prettyname] = fullname
-                print(prettyname)
+        # adds the files to the town to be written at the build stage
+        self.add_file(fullname)
 
         # update
         self.widgets['selected'].clear()
@@ -146,6 +170,29 @@ class Wizard(QWidget):
 
     ## signal helpers
 
+    def add_file(self, fullname):
+        # get partial file name and type
+        prettyname = fullname[(fullname.rfind('/') + 1):]
+        ftype = prettyname[:prettyname.find('.')]
+
+        # folder (recursive)
+        if os.path.isdir(fullname):
+            contents = glob.glob(fullname + '/*')
+            for i in contents: 
+                self.add_file(i)
+        # event
+        elif ftype == 'event':
+            # if not already selected
+            if prettyname not in self.town.events:
+                self.town.events[prettyname] = fullname
+                print(prettyname)
+        # townspeople  
+        elif ftype == 'townspeople':
+            if prettyname not in self.town.townspeople:
+                self.town.townspeople[prettyname] = fullname
+                print(prettyname)
+        else: self.status.showMessage("Not a valid file")
+
     def new_town(self):
         name, ok = QInputDialog.getText(self, "New Town","Town Name", QLineEdit.Normal, "")
         if ok:
@@ -157,6 +204,8 @@ class Wizard(QWidget):
         name, _ = QFileDialog.getOpenFileName(self, 'Open File', './towns')
         if name:
             self.town.load(name)
+            self.status.showMessage("Town loaded")
+        else: self.status.showMessage("No town loaded")
 
 if __name__ == '__main__':
     # QT IT UP
@@ -167,14 +216,3 @@ if __name__ == '__main__':
 
     # execute, clean up, and exit
     sys.exit(app.exec_())
-
-
-
-    '''
-    listWidget = QListWidget()
-
-    listWidget.addItems(tc.get_event_files())
-    listWidget.show()
-    '''
-
-
