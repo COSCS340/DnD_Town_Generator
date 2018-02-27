18ce0086650a6e198b0f55851675daa2ba874ded
Ben Johnson <thisguyistotallyben@gmail.com>
2018-02-27 12:55:20 -0500

    townwizar supports basic undo/redo actions

--- backend/townwizard.py
+++ backend/townwizard.py
@@ -31,6 +31,9 @@ class Wizard(QtWidgets.QMainWindow):
         # variables
         self.models = {}
         self.widgets = {}
+        self.menu = {}
+        self.undo = []
+        self.undone = []
         
         # add elements and setup layout
         self.setupInterface()
@@ -42,7 +45,8 @@ class Wizard(QtWidgets.QMainWindow):
     def setupInterface(self):
         # widgets
         self.widgets['tree'] = QTreeView()
-        self.widgets['selected'] = QListWidget()
+        self.widgets['stagebox'] = QGroupBox('Staging Area')
+        self.widgets['stage'] = QListWidget()
         self.widgets['new'] = QPushButton('New')
         self.widgets['edit'] = QPushButton('Edit')
         self.widgets['add'] = QPushButton('Add')
@@ -58,6 +62,7 @@ class Wizard(QtWidgets.QMainWindow):
         # layouts
         gridlayout = QGridLayout()
         vboxlayout = QGridLayout()
+        stagelayout = QVBoxLayout()
         
         # layout widgets
         mainbox = QWidget(self)
@@ -65,6 +70,7 @@ class Wizard(QtWidgets.QMainWindow):
         # set main layout
         self.setCentralWidget(mainbox)
         mainbox.setLayout(gridlayout)
+        self.widgets['stagebox'].setLayout(stagelayout)
         
         # set town info box layout
         self.widgets['info']['object'].setLayout(vboxlayout)
@@ -98,12 +104,14 @@ class Wizard(QtWidgets.QMainWindow):
         gridlayout.addWidget(self.widgets['new'],1,0)
         gridlayout.addWidget(self.widgets['edit'],1,1)
         gridlayout.addWidget(self.widgets['add'],1,2)
-        gridlayout.addWidget(self.widgets['selected'],0,3,2,1)
+        gridlayout.addWidget(self.widgets['stagebox'],0,3,2,1)
         gridlayout.addWidget(self.widgets['info']['object'],0,4,2,1)
         
         vboxlayout.addWidget(self.widgets['info']['townlabel'],0,0)
         vboxlayout.addWidget(self.widgets['info']['towntext'],0,1)
         vboxlayout.addWidget(self.widgets['info']['build'],2,0,1,2)
+
+        stagelayout.addWidget(self.widgets['stage'])
 
         # grid layout options
         gridlayout.setColumnStretch(3, 3)
@@ -119,19 +127,30 @@ class Wizard(QtWidgets.QMainWindow):
         
         # file menu
         file_menu = self.topbar.addMenu('File')
-        file_open = QtWidgets.QAction('Open', self)
-        file_close = QtWidgets.QAction('Close', self)
-        file_build = QtWidgets.QAction('Build', self)
-        file_menu.addAction(file_open)
-        file_menu.addAction(file_build)
+        self.menu['file_open'] = QtWidgets.QAction('Open', self)
+        self.menu['file_close'] = QtWidgets.QAction('Close', self)
+        self.menu['file_build'] = QtWidgets.QAction('Build', self)
+
+        file_menu.addAction(self.menu['file_open'])
+        file_menu.addAction(self.menu['file_build'])
         file_menu.addSeparator()
-        file_menu.addAction(file_close)
+        file_menu.addAction(self.menu['file_close'])
         
-        file_open.triggered.connect(self.loadsig)
-        file_build.triggered.connect(self.buildsig)
+        self.menu['file_open'].triggered.connect(self.loadsig)
+        self.menu['file_build'].triggered.connect(self.buildsig)
         
         # edit menu
         edit_menu = self.topbar.addMenu('Edit')
+        self.menu['edit_undo'] = QtWidgets.QAction('Undo', self)
+        self.menu['edit_redo'] = QtWidgets.QAction('Redo', self)
+
+        edit_menu.addAction(self.menu['edit_undo'])
+        edit_menu.addAction(self.menu['edit_redo'])
+        self.menu['edit_undo'].setDisabled(True)
+        self.menu['edit_redo'].setDisabled(True)
+
+        self.menu['edit_undo'].triggered.connect(self.undosig)
+        self.menu['edit_redo'].triggered.connect(self.redosig)
         
         # about menu
         about_menu = self.topbar.addMenu('Help')
@@ -142,13 +161,12 @@ class Wizard(QtWidgets.QMainWindow):
         # get file name
         index = self.widgets['tree'].currentIndex()
         fullname = self.models['tree'].filePath(index)
-        
+
         # adds the files to the town to be written at the build stage
         self.add_file(fullname)
 
-        # update
-        self.widgets['selected'].clear()
-        self.widgets['selected'].addItems(self.town.events.keys() | self.town.townspeople.keys())
+        # update staging view
+        self.update_staging()
     
     def newsig(self):
         if self.town.active:
@@ -171,18 +189,55 @@ class Wizard(QtWidgets.QMainWindow):
     def buildsig(self):
         # check if town loaded
         if not self.town.active:
-            print(self.widgets['info']['towntext'].text())
             self.town.new(self.widgets['info']['towntext'].text())
 
         # save it
         name, _ = QFileDialog.getSaveFileName(self, 'Save File', './towns')
         if name != '':
-            print(name[-5:])
             if name[-5:] != '.json': name = name + '.json'
             self.town.build(name)
             self.status.showMessage('Saved to ' + name)
 
+    def undosig(self):
+        # get change
+        change = self.undo.pop()
+        self.undone.append(change)
+
+        if change['type'] == 'addevent':
+            self.town.events.pop(change['prettyname'])
+        elif change['type'] == 'addtownspeople':
+            self.town.townspeople.pop(change['prettyname'])
+
+        # disability claims
+        self.menu['edit_redo'].setDisabled(False)
+        if len(self.undo) == 0: self.menu['edit_undo'].setDisabled(True)
+
+        # update staging view
+        self.update_staging()
+
+    def redosig(self):
+        # get change
+        change = self.undone.pop()
+        self.undo.append(change)
+
+        if change['type'] == 'addevent':
+            self.town.events[change['prettyname']] = change['fullname']
+        elif change['type'] == 'addtownspeople':
+            self.town.townspeople[change['prettyname']] = change['fullname']
+
+        # disability claims
+        self.menu['edit_undo'].setDisabled(False)
+        if len(self.undone) == 0: self.menu['edit_redo'].setDisabled(True)
+
+        # update staging view
+        self.update_staging()
+
     ## signal helpers
+
+    def update_staging(self):
+        self.widgets['stage'].clear()
+        self.widgets['stage'].addItems(self.town.events.keys() | self.town.townspeople.keys())
+        
 
     def add_file(self, fullname):
         # get partial (pretty) file name and type
@@ -202,15 +257,15 @@ class Wizard(QtWidgets.QMainWindow):
             # if not already selected
             if prettyname not in self.town.events:
                 self.town.events[prettyname] = fullname
-                print(prettyname)
                 self.status.showMessage('Event added')
+                self.log_change('addevent', prettyname, fullname)
             else: self.status.showMessage('Event already added')
         # townspeople  
         elif ftype == 'townspeople':
             if prettyname not in self.town.townspeople:
                 self.town.townspeople[prettyname] = fullname
-                print(prettyname)
                 self.status.showMessage('Townspeople added')
+                self.log_change('addtownspeople', prettyname, fullname)
             else: self.status.showMessage('Townspeople already added')
         else: self.status.showMessage('Not a valid file')
 
@@ -221,6 +276,20 @@ class Wizard(QtWidgets.QMainWindow):
             self.status.showMessage("Town loaded")
             self.widgets['info']['towntext'].setText(self.town.data['name'])
         else: self.status.showMessage("No town loaded")
+
+    def log_change(self, ctype, prettyname, fullname):
+        # setup
+        self.undone.clear()
+        change = {}
+        # add to changes
+        change['type'] = ctype
+        change['prettyname'] = prettyname
+        change['fullname'] = fullname
+        self.undo.append(change)
+
+        # disability claims
+        self.menu['edit_undo'].setDisabled(False)
+        if len(self.undone) == 0: self.menu['edit_redo'].setDisabled(True)
 
 if __name__ == '__main__':
     # QT IT UP
