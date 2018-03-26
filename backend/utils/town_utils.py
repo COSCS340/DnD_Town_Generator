742dc2604ef61b54e38f6ba6e16d62a6aafb30d2
Ben Johnson <thisguyistotallyben@gmail.com>
2018-03-25 20:43:57 -0400

    mergerating branches

--- backend/utils/town_utils.py
+++
@@ -1,8 +0,0 @@
-class dice:
-    def __init__(self, numsides=20):
-        self.numsides = numsides
-        print(self.numsides)
-
-if __name__ == "__main__":
-    print("TOWN UTILS")
-    die = dice()
