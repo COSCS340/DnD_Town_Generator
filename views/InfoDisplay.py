c39ffc4d3137114a29ff5b978ad7c7ffb043a22a
dsande30 <dsande30@vols.utk.edu>
2018-04-17 23:47:32 -0400

    Attempting to make something show

--- views/InfoDisplay.py
+++ views/InfoDisplay.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import *
 
 from utils.multiview import View
@@ -11,6 +12,7 @@ class InfoDisplay(View):
 
         self.layouts = {}
         self.widgets = {}
+        self.boxes ={}
 
         self.citizens = self.parent.access('wizard').town.citizens
 
@@ -19,22 +21,29 @@ class InfoDisplay(View):
     def setupInterface(self):
         # widgets
         self.widgets['scrollarea'] = QScrollArea()
-        self.widgets['test'] = QPushButton('Press me')
+        self.widgets['refresh'] = QPushButton('Refresh')
 
-        self.widgets['test'].clicked.connect(self.update_list)
+        self.widgets['refresh'].clicked.connect(self.refresh)
 
         # layouts
         self.layouts['main'] = QVBoxLayout()
         self.set_layout(self.layouts['main'])
 
+        self.layouts['innerbox'] = QVBoxLayout()
+        self.widgets['scrollarea'].setWidget(self.layouts['innerbox'].widget())
+
         # build
         self.layouts['main'].addWidget(self.widgets['scrollarea'])
-        self.layouts['main'].addWidget(self.widgets['test'])
+        self.layouts['main'].addWidget(self.widgets['refresh'])
 
     def sig(self):
         sys.exit()
 
     # function to call when town created
-    def update_list(self):
+    def refresh(self):
+        self.citizens = self.parent.access('wizard').town.citizens
         for i in self.citizens:
-            print(f'{i.fname} {i.lname}')
+            self.boxes[i] = QTextEdit(self)
+            self.boxes[i].setTextBackgroundColor(QColor(128, 128, 128))
+            self.boxes[i].setText(f'{i.fname} {i.lname}')
+            self.layouts['innerbox'].addWidget(self.boxes[i])
